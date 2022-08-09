6a3fd6ab938ba14f235f364570712bb8c23a445a
Juan <jdehoyos555@gmail.com>
2022-08-09 17:17:34 -0500

    Reto_11

---
+++ task4.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include "task4.h"
+
+
+void task4(){
+    enum class Task1States{
+        INIT,
+        WAIT_TO_TOGGLE_LED
+    };
+    static Task1States task1State = Task1States::INIT;
+    static uint32_t lasTime;
+    static constexpr uint32_t INTERVAL = 250;
+    static constexpr uint8_t ledBlue = 27;
+    static bool ledStatus = false;
+
+    switch(task1State){
+        case Task1States::INIT:{
+            pinMode(ledBlue,OUTPUT);
+            lasTime = millis();
+            task1State = Task1States::WAIT_TO_TOGGLE_LED;
+            break;
+        }
+
+        case Task1States::WAIT_TO_TOGGLE_LED:{
+            // evento 1:
+            uint32_t currentTime = millis();
+            if( (currentTime - lasTime) >= INTERVAL ){
+                lasTime = currentTime;
+                digitalWrite(ledBlue,ledStatus);
+                ledStatus = !ledStatus;
+            }
+            break;
+        }
+
+        default:{
+            break;
+        }
+    }
+
+}
